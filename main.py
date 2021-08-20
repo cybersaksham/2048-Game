524850012bdc5195024fe7e2121ec4165ebc4360
Saksham <2020UCP1801@mnit.ac.in>
2021-08-20 20:59:21 +0530

    preview window complete

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import pygame
 import sys
 from pygame import mixer
 from pygame.locals import *
+from Scripts.variables import *
 
 # INITIALIZATION
 pygame.init()
@@ -13,10 +14,22 @@ SCREENHEIGHT = 600
 SCREEN = pygame.display.set_mode((SCREENWIDTH, SCREENHEIGHT))
 pygame.display.set_caption("2048 Game")
 
+# FONT
+FONT = pygame.font.SysFont(None, 40)
+
+# IMAGES
+PREVIEW_WINDOW = pygame.transform.scale(pygame.image.load("Gallery/Images/images.jpeg"), (SCREENWIDTH, SCREENHEIGHT))
+
+
+# PREVIEW WINDOW
+def prevWindow():
+    SCREEN.blit(PREVIEW_WINDOW, (0, 0))
+    SCREEN.blit(FONT.render("Click anywhere to start", True, WHITE), [SCREENWIDTH / 4, SCREENHEIGHT / 2 - 45])
+
 
 # GAME LOOP
 def mainLoop():
-    SCREEN.blit(pygame.transform.scale(pygame.image.load("Gallery/Images/images.jpeg"), (600, 600)), (0, 0))
+    prevWindow()
     for event in pygame.event.get():
         if event.type == QUIT:
             pygame.quit()
