6a0e00bac747fc5e9bb10a5406cc047999c74e2d
Saksham <2020UCP1801@mnit.ac.in>
2021-08-21 14:10:15 +0530

    icon added

--- Scripts/screen.py
+++ Scripts/screen.py
@@ -5,3 +5,4 @@ SCREENWIDTH = 600
 SCREENHEIGHT = 600
 SCREEN = pygame.display.set_mode((SCREENWIDTH, SCREENHEIGHT))
 pygame.display.set_caption("2048 Game")
+pygame.display.set_icon(pygame.image.load("Gallery/icon.ico"))
